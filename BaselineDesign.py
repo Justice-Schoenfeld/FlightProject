9691c7e784c485a6b663891c2937aa4cf46dcaad
Cory <cory.goates@aggiemail.usu.edu>
2019-03-06 14:18:07 -0700

    Added baseline .json file.

--- BaselineDesign.py
+++ BaselineDesign.py
@@ -6,3 +6,14 @@ Created on Tue Mar  5 11:11:37 2019
 """
 
 import numpy as np
+import machup.MU as MU
+import matplotlib.pyplot as plt
+
+
+baseline = MU.MachUp("BaselineDesign.json")
+
+atmos_state = {"V_mag":10,
+              "rho":0.0023769,
+              "alpha": 0,
+              "beta":0}
+
